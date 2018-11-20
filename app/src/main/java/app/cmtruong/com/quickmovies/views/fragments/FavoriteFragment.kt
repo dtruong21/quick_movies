bde1895b6720f1ebeac8572db3fbeb61bf897b8b
Minh Truong <congminh.truong.ext@parrot.com>
2018-11-20 17:21:06 +0100

    init favorite fragment

--- app/src/main/java/app/cmtruong/com/quickmovies/views/fragments/FavoriteFragment.kt
+++ app/src/main/java/app/cmtruong/com/quickmovies/views/fragments/FavoriteFragment.kt
@@ -1,8 +1,13 @@
 package app.cmtruong.com.quickmovies.views.fragments
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import app.cmtruong.com.quickmovies.R
 import kotlinx.android.synthetic.main.extras_fragment_container.*
+import timber.log.Timber
 
 /**
  * @author davidetruong
@@ -15,19 +20,24 @@ class FavoriteFragment: Fragment() {
         fun getInstance(): FavoriteFragment = FavoriteFragment()
     }
 
-    fun showMessageError() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Timber.d("Favorite fragment created ...")
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+
+     private fun showMessageError() {
         pb_movie.visibility = View.GONE
         movie_error.visibility = View.VISIBLE
         rv_movies.visibility = View.GONE
     }
 
-    fun showResults(){
+    private fun showResults(){
         pb_movie.visibility = View.GONE
         movie_error.visibility = View.GONE
         rv_movies.visibility = View.VISIBLE
     }
 
-    fun loadingData(){
+    private fun loadingData(){
         pb_movie.visibility = View.VISIBLE
         movie_error.visibility = View.GONE
         rv_movies.visibility = View.GONE
