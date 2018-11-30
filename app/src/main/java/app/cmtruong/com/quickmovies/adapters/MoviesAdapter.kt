b34c1fa364c489c7362c76e6106748f620b733e9
Minh Truong <congminh.truong.ext@parrot.com>
2018-11-30 13:29:36 +0100

    update gradle

--- app/src/main/java/app/cmtruong/com/quickmovies/adapters/MoviesAdapter.kt
+++ app/src/main/java/app/cmtruong/com/quickmovies/adapters/MoviesAdapter.kt
@@ -29,7 +29,7 @@ class MoviesAdapter(private val context: Context?, private val movies: List<Movi
         val view: View = inflater.inflate(R.layout.movies_item, parent, false)
         Timber.tag(TAG)
         Timber.d("Adapter created")
-        return MoviesViewHolder(view)x
+        return MoviesViewHolder(view)
     }
 
     override fun getItemCount(): Int {
