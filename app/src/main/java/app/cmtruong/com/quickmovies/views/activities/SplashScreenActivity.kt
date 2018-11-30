0eafec0e2f27b83a82ac4b5fc7c7981403037108
Minh Truong <congminh.truong.ext@parrot.com>
2018-11-30 09:40:32 +0100

    implement splashscreen function

--- app/src/main/java/app/cmtruong/com/quickmovies/views/activities/SplashScreenActivity.kt
+++ app/src/main/java/app/cmtruong/com/quickmovies/views/activities/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package app.cmtruong.com.quickmovies.views.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import app.cmtruong.com.quickmovies.R
@@ -14,5 +15,12 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        startMainScreen()
+    }
+
+    private fun startMainScreen(){
+        val intent: Intent? = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
